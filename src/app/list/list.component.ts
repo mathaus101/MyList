c91e4afa9ef01af86c32376afbf15a4da927e71f
mathus101 <mattjamesbrooks@gmail.com>
2019-06-25 18:53:11 +1000

    added angular animations

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -5,14 +5,17 @@ import { CdkDragDrop } from '@angular/cdk/drag-drop';
 import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import {MatSnackBar} from '@angular/material/snack-bar';
 
-// import {
-//   trigger,
-//   state,
-//   style,
-//   animate,
-//   transition,
-//   // ...
-// } from '@angular/animations';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  query,
+  stagger,
+  animateChild
+  // ...
+} from '@angular/animations';
 
 export interface DialogData
 {
@@ -24,7 +27,35 @@ export interface DialogData
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
+  animations: [
+
+    trigger('items', [
+
+      transition( ':enter', [
+      style({ transform: 'scale(0.5)', opacity: 0 }),  // initial
+      animate('1s cubic-bezier(.8, -0.6, 0.2, 1.5)', 
+        style({ transform: 'scale(1)', opacity: 1 }))  // final])
+      ]),
+      
+      transition( ':leave', [
+        style({ transform: 'scale(1)', opacity: 1, height: '*' }),
+        animate('1s cubic-bezier(.8, -0.6, 0.2, 1.5)', 
+         style({ 
+           transform: 'scale(0.5)', opacity: 0, 
+           height: '0px', margin: '0px' 
+         })) 
+      ])      
+    ]),
+
+    trigger('list', [
+      transition(':enter', [
+        query('@items', stagger(150, animateChild()))
+      ])
+    ]),
+
+
+  ]
 })
 export class ListComponent implements OnInit {
 
